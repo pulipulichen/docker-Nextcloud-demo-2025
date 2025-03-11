09b09ef7cef62b907e492647ea04d37e3584ec2c
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-12 01:14:14 +0800

    西元2025年03月12日 (週三) 01時14分14秒 CST

---
+++ nextcloud/docker-entrypoint-hooks.d/post-installation/link_html.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# cp -rf /html/* /var/www/html/
+
+#!/bin/bash
+
+# 定義目標目錄
+# TARGET_DIRS=("core" "config", "lib")
+# TARGET_DIRS=("core" "config")
+TARGET_DIRS=("core" "dist")
+
+# 迭代處理每個目錄
+for DIR in "${TARGET_DIRS[@]}"; do
+    if [ -d "/html/$DIR" ]; then
+        # 如果 /var/www/html/$DIR 存在，則刪除
+        if [ -e "/var/www/html/$DIR" ]; then
+            rm -rf "/var/www/html/$DIR"
+        fi
+        # 建立符號連結
+        ln -s "/html/$DIR" "/var/www/html/$DIR"
+        chown www-data:www-data -R /html/$DIR
+        echo "Linking is successful: /html/$DIR -> /var/www/html/$DIR"
+    else
+        echo "/html/$DIR is not existed. Skip."
+    fi
+done

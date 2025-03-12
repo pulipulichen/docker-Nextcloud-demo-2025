5d5d3b2b90edf361186f2b869f58210b62c6f86c
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-13 02:17:18 +0800

    處理APP MENU順序的問題：apporder版本不合；先把metadata開起來吧。西元2025年03月13日 (週四) 02時17分18秒 CST

--- nextcloud_server/docker-entrypoint-hooks.d/post-installation/10.enable_apporder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# /var/www/html/occ app:enable apporder
--- nextcloud_server/docker-entrypoint-hooks.d/post-installation/10.enable_metadata.sh
+++ nextcloud_server/docker-entrypoint-hooks.d/post-installation/10.enable_metadata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-# /var/www/html/occ app:enable metadata
+/var/www/html/occ app:enable metadata
+
+# sudo -u www-data /var/www/html/occ app:enable metadata

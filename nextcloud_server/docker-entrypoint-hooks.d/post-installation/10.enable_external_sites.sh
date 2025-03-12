d2366db42ab57b968ce459b25669fa30bbea9b2b
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-12 23:58:22 +0800

    修復損毀的保存庫中。還原到可能可行的版本，再重啟看看。西元2025年03月12日 (週三) 23時58分22秒 CST

--- nextcloud_server/docker-entrypoint-hooks.d/post-installation/10.enable_external_sites.sh
+++ nextcloud_server/docker-entrypoint-hooks.d/post-installation/10.enable_external_sites.sh
@@ -3,7 +3,7 @@
 /var/www/html/occ app:enable external
 
 if [ -n "$NEXTCLOUD_EXTERNAL_SITES" ]; then
-  /var/www/html/occ config:app:set external sites --value="$NEXTCLOUD_EXTERNAL_SITES"
+  /var/www/html/occ config:app:set external sites --value=$NEXTCLOUD_EXTERNAL_SITES
 fi
 
 # sudo -u www-data /var/www/html/occ config:app:set external sites --value='{"1":{"id":1,"name":"Home","url":"https://blog.pulipuli.info","icon":"/apps/files/img/app.svg","lang":"","redirect":false}}'

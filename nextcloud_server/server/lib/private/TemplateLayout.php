eb84b75da5e52ac7c850e149aff8cdf0193917a5
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-13 01:38:39 +0800

    來修正樣式表沒有載入的問題：有偵測到同步，但沒有複製過去嗎？嘗試改變擁有者看看，應該是權限的問題。西元2025年03月13日 (週四) 01時38分39秒 CST

--- nextcloud_server/server/lib/private/TemplateLayout.php
+++ nextcloud_server/server/lib/private/TemplateLayout.php
@@ -286,7 +286,7 @@ class TemplateLayout extends \OC_Template {
 			$this->append('cssfiles', '/custom_theme/css/guest-style.css'. '?v=' . substr($suffix, 3));
 		}
 		$this->append('cssfiles', '/custom_theme/css/global-style.css'. '?v=' . substr($suffix, 3)); 
-		//
+		
 		$request = \OCP\Server::get(IRequest::class);
 		if ($request->isUserAgent([Request::USER_AGENT_CLIENT_IOS, Request::USER_AGENT_SAFARI, Request::USER_AGENT_SAFARI_MOBILE])) {
 			// Prevent auto zoom with iOS but still allow user zoom
